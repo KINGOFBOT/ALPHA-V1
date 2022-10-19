114ace417c7bddcfed8dc7cd027c54d92c892225
TimashaBuddilax <116003518+TimashaBuddilax@users.noreply.github.com>
2022-10-19 14:59:09 +0100

    Update Alphamd.js

--- Alphamd.js
+++ Alphamd.js
@@ -1,4 +1,5 @@
 require('./settings')
+require('.language/EN')
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@adiwajshing/baileys')
 const fs = require('fs')
 const util = require('util')
