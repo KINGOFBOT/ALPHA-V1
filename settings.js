f97cf8a7aa3bf1a6c48fa00e5b53ac82541c91a0
TimashaBuddilax <116003518+TimashaBuddilax@users.noreply.github.com>
2022-10-19 15:08:54 +0100

    bot main commands aded..bot realise soon

--- settings.js
+++ settings.js
@@ -1,1 +1,67 @@
-const _0xd59e81=_0x4ea2;function _0x4ea2(_0x5a763b,_0x56b5d4){const _0x1c371b=_0x1c37();return _0x4ea2=function(_0x4ea27f,_0x4813c5){_0x4ea27f=_0x4ea27f-0xab;let _0x227aba=_0x1c371b[_0x4ea27f];return _0x227aba;},_0x4ea2(_0x5a763b,_0x56b5d4);}(function(_0xa757d,_0x40fa4c){const _0x5f8671=_0x4ea2,_0x22cd86=_0xa757d();while(!![]){try{const _0x2ac221=-parseInt(_0x5f8671(0xcc))/0x1*(parseInt(_0x5f8671(0xb1))/0x2)+parseInt(_0x5f8671(0xe1))/0x3+parseInt(_0x5f8671(0xdd))/0x4+-parseInt(_0x5f8671(0xe0))/0x5*(-parseInt(_0x5f8671(0xd0))/0x6)+parseInt(_0x5f8671(0xb5))/0x7+-parseInt(_0x5f8671(0xd6))/0x8*(-parseInt(_0x5f8671(0xdb))/0x9)+-parseInt(_0x5f8671(0xbb))/0xa;if(_0x2ac221===_0x40fa4c)break;else _0x22cd86['push'](_0x22cd86['shift']());}catch(_0x1c1973){_0x22cd86['push'](_0x22cd86['shift']());}}}(_0x1c37,0xc2ffc));const fs=require('fs'),chalk=require(_0xd59e81(0xda));function _0x1c37(){const _0x506937=['premium','94784506970','Wait\x20A\x20minute...','105199adOUOY','Your\x20Daily\x20Limit\x20Has\x20Expired,\x20The\x20Limit\x20Will\x20Be\x20Reset\x20Every\x2012\x20Hours','owner','log','7374tuDOlZ','redBright','readFileSync','APIs','sanzychan01','unwatchFile','197896OlpASh','Features\x20Used\x20Only\x20For\x20Private\x20Chat!','thumb','rpg','chalk','18jxusoZ','APIKeys','6244884KnpxtI','resolve','Error!\x20Maybe\x20Api\x20Key\x20Is\x20Expired🤔!','4835BhANjM','3587520UNSedl','https://zenzapis.xyz','Done\x20✓','Achi\x20Frenando','India,\x20South\x20India,\x20Kerala','ownernomer','author','6wNdAgp','watchFile','region','https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=','10886638hMPbQx','mess','Infinity','ALPHA\x20MD\x20BOT','ALPHA','https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=','44358990yBecrO','cache','fluming','myweb','This\x20Feature\x20Is\x20Only\x20For\x20Owner!','sessionName','botname','This\x20Feature\x20Is\x20Only\x20For\x20Bot','https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=','Feature\x20Used\x20Only\x20For\x20Groups!','flasmurf','limitawal','footer','ownername'];_0x1c37=function(){return _0x506937;};return _0x1c37();}global[_0xd59e81(0xd3)]={'zenz':_0xd59e81(0xab)},global[_0xd59e81(0xdc)]={'https://zenzapis.xyz':_0xd59e81(0xd4)},global[_0xd59e81(0xce)]=[_0xd59e81(0xca)],global[_0xd59e81(0xc9)]=['94784506970'],global[_0xd59e81(0xaf)]=_0xd59e81(0xca),global[_0xd59e81(0xc8)]='Achi\x20Frenando',global[_0xd59e81(0xc1)]=_0xd59e81(0xb8),global[_0xd59e81(0xc7)]='©ᴀʟᴘʜᴀ\x20ʙᴏᴛ\x20ʙᴇᴛᴀ\x20ᴇᴅɪᴛɪᴏɴ',global['ig']='',global[_0xd59e81(0xb3)]=_0xd59e81(0xae),global['sc']='',global[_0xd59e81(0xbe)]='',global['packname']=_0xd59e81(0xb9),global[_0xd59e81(0xb0)]=_0xd59e81(0xad),global[_0xd59e81(0xc0)]='session',global['prefa']=['','!','.','🐦','🐤','🗿'],global['sp']='⭔',global[_0xd59e81(0xb6)]={'success':_0xd59e81(0xac),'admin':'This\x20Feature\x20Is\x20Only\x20For\x20Admin!','botAdmin':'Bot\x20Must\x20Be\x20Admin\x20First!','owner':_0xd59e81(0xbf),'group':_0xd59e81(0xc4),'private':_0xd59e81(0xd7),'bot':_0xd59e81(0xc2),'wait':_0xd59e81(0xcb),'error':_0xd59e81(0xdf),'endLimit':_0xd59e81(0xcd)},global[_0xd59e81(0xc6)]={'premium':_0xd59e81(0xb7),'free':0xc,'monayawal':0x3e8},global[_0xd59e81(0xd9)]={'darahawal':0x64,'besiawal':0xf,'goldawal':0xa,'emeraldawal':0x5,'umpanawal':0x5,'potionawal':0x1},global[_0xd59e81(0xd8)]=fs[_0xd59e81(0xd2)]('./GojoMedia/gojo.jpg'),global['flaming']=_0xd59e81(0xb4),global[_0xd59e81(0xbd)]=_0xd59e81(0xc3),global['flarun']=_0xd59e81(0xba),global[_0xd59e81(0xc5)]='https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=';let file=require[_0xd59e81(0xde)](__filename);fs[_0xd59e81(0xb2)](file,()=>{const _0x4dbba9=_0xd59e81;fs[_0x4dbba9(0xd5)](file),console[_0x4dbba9(0xcf)](chalk[_0x4dbba9(0xd1)]('Update\x27'+__filename+'\x27')),delete require[_0x4dbba9(0xbc)][file],require(file);});
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+}
+
+//customize settings\\
+global.owner = ['94784506970']
+global.premium = ['94784506970']
+global.ownernomer = '94784506970'
+global.ownername = 'Achi Fernando'
+global.botname = 'ALPHA-MD'
+global.footer = 'ᴀʟᴘʜᴀ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ'
+global.ig = 'https://github.com/nexusNw'
+global.region = 'India, South India, Kerala'
+global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
+global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.packname = 'ALPHA-MD'
+global.author = 'D.U.A.N.FERNANDO'
+global.sessionName = 'session'
+global.BRANCH = 'main'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: 'Wait A minute...',
+    error: 'Error! Maybe Api Key Is Expired🤔!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./GojoMedia/gojo.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
